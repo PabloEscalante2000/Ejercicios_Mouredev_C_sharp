6cd6080a1878344f32f9bc9bf668559711b43ffc
Pablo <pabloescalante200027@gmail.com>
2022-09-08 20:59:43 -0500

    Ejercicio 2 - Resuelto

---
+++ Ejercicios_Mourodev/Form2.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNom1 = new System.Windows.Forms.TextBox();
+            this.txtNom2 = new System.Windows.Forms.TextBox();
+            this.btnAnagrama = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Palabra 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Palabra 2:";
+            // 
+            // txtNom1
+            // 
+            this.txtNom1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNom1.Location = new System.Drawing.Point(137, 35);
+            this.txtNom1.Name = "txtNom1";
+            this.txtNom1.Size = new System.Drawing.Size(100, 20);
+            this.txtNom1.TabIndex = 2;
+            // 
+            // txtNom2
+            // 
+            this.txtNom2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNom2.Location = new System.Drawing.Point(137, 71);
+            this.txtNom2.Name = "txtNom2";
+            this.txtNom2.Size = new System.Drawing.Size(100, 20);
+            this.txtNom2.TabIndex = 3;
+            // 
+            // btnAnagrama
+            // 
+            this.btnAnagrama.Location = new System.Drawing.Point(80, 154);
+            this.btnAnagrama.Name = "btnAnagrama";
+            this.btnAnagrama.Size = new System.Drawing.Size(118, 23);
+            this.btnAnagrama.TabIndex = 4;
+            this.btnAnagrama.Text = "¿Son anagramas?";
+            this.btnAnagrama.UseVisualStyleBackColor = true;
+            this.btnAnagrama.Click += new System.EventHandler(this.btnAnagrama_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 213);
+            this.Controls.Add(this.btnAnagrama);
+            this.Controls.Add(this.txtNom2);
+            this.Controls.Add(this.txtNom1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNom1;
+        private System.Windows.Forms.TextBox txtNom2;
+        private System.Windows.Forms.Button btnAnagrama;
+    }
+}
---
+++ Ejercicios_Mourodev/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnagrama_Click(object sender, EventArgs e)
+        {
+            double n1,n2;
+
+            if (txtNom1.Text.Trim() == String.Empty | txtNom2.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Tiene que ingresarse un nombre en cada casilla", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (Double.TryParse(txtNom1.Text, out n1) | Double.TryParse(txtNom2.Text, out n2))
+            {
+                MessageBox.Show("Tiene que ingresar cadenas de texto, NO NUMEROS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            { 
+                String nom1 = txtNom1.Text.Trim();
+                String nom2 = txtNom2.Text.Trim();
+
+                // Se aplica el Anagrama
+
+                if (Anagrama(nom1, nom2))
+                {
+                    MessageBox.Show("Son anagramas!", "ÉXITO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No son anagramas...", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        bool Anagrama(String nom1, String nom2)
+        {
+            if (nom1.Length != nom2.Length)
+            {
+                return false;
+            }
+            else
+            {
+                List<Char> list1 = new List<Char>();
+                List<Char> list2 = new List<Char>();
+
+                foreach (Char ch in nom1)
+                {
+                    list1.Add(ch);
+                }
+                foreach (Char ch in nom2)
+                {
+                    list2.Add(ch);
+                }
+
+                var c = 0;
+
+                while (c < list1.Count)
+                {
+                    for (int i = 0; i < list2.Count; i++)
+                    {
+                        if (list1[c] == list2[i])
+                        {
+
+                            c++;
+                            break;
+                        }
+
+                        if (i == list2.Count - 1)
+                        { 
+                            return false;
+                        }
+                    }
+                }
+
+                return true;    
+            }
+        }
+    }
+}
