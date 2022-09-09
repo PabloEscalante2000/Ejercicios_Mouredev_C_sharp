91baaed80f143b180fa1863f5bd6570c5b813c0a
Pablo <pabloescalante200027@gmail.com>
2022-09-08 22:09:29 -0500

    Ejercicio 4 - Resuelto

---
+++ Ejercicios_Mourodev/Form4.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrimo = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Da un número:";
+            // 
+            // txtPrimo
+            // 
+            this.txtPrimo.Location = new System.Drawing.Point(139, 41);
+            this.txtPrimo.Name = "txtPrimo";
+            this.txtPrimo.Size = new System.Drawing.Size(100, 20);
+            this.txtPrimo.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(59, 89);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "¿Es Primo?";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 133);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPrimo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "¿ Es Primo ?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrimo;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ Ejercicios_Mourodev/Form4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(txtPrimo.Text);
+
+            if (primo(num))
+            {
+                MessageBox.Show("Es un Primo!!", "ACERTASTE!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No es un primo, sorry :'v", "UPS!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        bool primo(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            else if (num == 2)
+            { 
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    { 
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
