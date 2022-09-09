6ed282856a0fc48a341e23d222aa17e8abc2fc0d
Pablo <pabloescalante200027@gmail.com>
2022-09-09 09:11:52 -0500

    Ejercicio 5 - Resuelto

--- Ejercicios_Mourodev/Form5.Designer.cs
+++ Ejercicios_Mourodev/Form5.Designer.cs
@@ -28,82 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblAncho = new System.Windows.Forms.Label();
+            this.lblAlto = new System.Windows.Forms.Label();
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.txtAlto = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rbTriangulo = new System.Windows.Forms.RadioButton();
-            this.rbRectángulo = new System.Windows.Forms.RadioButton();
             this.rbCuadrado = new System.Windows.Forms.RadioButton();
+            this.rbRectángulo = new System.Windows.Forms.RadioButton();
+            this.rbTriangulo = new System.Windows.Forms.RadioButton();
+            this.btnArea = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblAncho
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Ancho:";
+            this.lblAncho.AutoSize = true;
+            this.lblAncho.Location = new System.Drawing.Point(35, 32);
+            this.lblAncho.Name = "lblAncho";
+            this.lblAncho.Size = new System.Drawing.Size(41, 13);
+            this.lblAncho.TabIndex = 0;
+            this.lblAncho.Text = "Ancho:";
             // 
-            // label2
+            // lblAlto
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Alto:";
+            this.lblAlto.AutoSize = true;
+            this.lblAlto.Location = new System.Drawing.Point(38, 61);
+            this.lblAlto.Name = "lblAlto";
+            this.lblAlto.Size = new System.Drawing.Size(28, 13);
+            this.lblAlto.TabIndex = 1;
+            this.lblAlto.Text = "Alto:";
             // 
-            // textBox1
+            // txtAncho
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtAncho.Location = new System.Drawing.Point(99, 24);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(100, 20);
+            this.txtAncho.TabIndex = 2;
             // 
-            // textBox2
+            // txtAlto
             // 
-            this.textBox2.Location = new System.Drawing.Point(99, 53);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtAlto.Location = new System.Drawing.Point(99, 53);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(100, 20);
+            this.txtAlto.TabIndex = 3;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.rbCuadrado);
             this.groupBox1.Controls.Add(this.rbRectángulo);
             this.groupBox1.Controls.Add(this.rbTriangulo);
-            this.groupBox1.Location = new System.Drawing.Point(295, 24);
+            this.groupBox1.Location = new System.Drawing.Point(38, 111);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.Size = new System.Drawing.Size(161, 100);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Selecciona la figura";
-            // 
-            // rbTriangulo
-            // 
-            this.rbTriangulo.AutoSize = true;
-            this.rbTriangulo.Location = new System.Drawing.Point(19, 19);
-            this.rbTriangulo.Name = "rbTriangulo";
-            this.rbTriangulo.Size = new System.Drawing.Size(69, 17);
-            this.rbTriangulo.TabIndex = 0;
-            this.rbTriangulo.TabStop = true;
-            this.rbTriangulo.Text = "Triángulo";
-            this.rbTriangulo.UseVisualStyleBackColor = true;
-            // 
-            // rbRectángulo
-            // 
-            this.rbRectángulo.AutoSize = true;
-            this.rbRectángulo.Location = new System.Drawing.Point(19, 43);
-            this.rbRectángulo.Name = "rbRectángulo";
-            this.rbRectángulo.Size = new System.Drawing.Size(80, 17);
-            this.rbRectángulo.TabIndex = 1;
-            this.rbRectángulo.TabStop = true;
-            this.rbRectángulo.Text = "Rectángulo";
-            this.rbRectángulo.UseVisualStyleBackColor = true;
             // 
             // rbCuadrado
             // 
@@ -115,19 +94,56 @@
             this.rbCuadrado.TabStop = true;
             this.rbCuadrado.Text = "Cuadrado";
             this.rbCuadrado.UseVisualStyleBackColor = true;
+            this.rbCuadrado.CheckedChanged += new System.EventHandler(this.rbCuadrado_CheckedChanged);
+            // 
+            // rbRectángulo
+            // 
+            this.rbRectángulo.AutoSize = true;
+            this.rbRectángulo.Location = new System.Drawing.Point(19, 43);
+            this.rbRectángulo.Name = "rbRectángulo";
+            this.rbRectángulo.Size = new System.Drawing.Size(80, 17);
+            this.rbRectángulo.TabIndex = 1;
+            this.rbRectángulo.TabStop = true;
+            this.rbRectángulo.Text = "Rectángulo";
+            this.rbRectángulo.UseVisualStyleBackColor = true;
+            this.rbRectángulo.CheckedChanged += new System.EventHandler(this.rbRectángulo_CheckedChanged);
+            // 
+            // rbTriangulo
+            // 
+            this.rbTriangulo.AutoSize = true;
+            this.rbTriangulo.Location = new System.Drawing.Point(19, 19);
+            this.rbTriangulo.Name = "rbTriangulo";
+            this.rbTriangulo.Size = new System.Drawing.Size(69, 17);
+            this.rbTriangulo.TabIndex = 0;
+            this.rbTriangulo.TabStop = true;
+            this.rbTriangulo.Text = "Triángulo";
+            this.rbTriangulo.UseVisualStyleBackColor = true;
+            this.rbTriangulo.CheckedChanged += new System.EventHandler(this.rbTriangulo_CheckedChanged);
+            // 
+            // btnArea
+            // 
+            this.btnArea.Location = new System.Drawing.Point(38, 229);
+            this.btnArea.Name = "btnArea";
+            this.btnArea.Size = new System.Drawing.Size(75, 23);
+            this.btnArea.TabIndex = 5;
+            this.btnArea.Text = "Área";
+            this.btnArea.UseVisualStyleBackColor = true;
+            this.btnArea.Click += new System.EventHandler(this.btnArea_Click);
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 450);
+            this.ClientSize = new System.Drawing.Size(240, 301);
+            this.Controls.Add(this.btnArea);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAlto);
+            this.Controls.Add(this.txtAncho);
+            this.Controls.Add(this.lblAlto);
+            this.Controls.Add(this.lblAncho);
             this.Name = "Form5";
-            this.Text = "Form5";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Área de figuras Geométricas";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -137,13 +153,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblAncho;
+        private System.Windows.Forms.Label lblAlto;
+        private System.Windows.Forms.TextBox txtAncho;
+        private System.Windows.Forms.TextBox txtAlto;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rbCuadrado;
         private System.Windows.Forms.RadioButton rbRectángulo;
         private System.Windows.Forms.RadioButton rbTriangulo;
+        private System.Windows.Forms.Button btnArea;
     }
 }
--- Ejercicios_Mourodev/Form5.cs
+++ Ejercicios_Mourodev/Form5.cs
@@ -15,6 +15,69 @@ namespace Ejercicios_Mourodev
         public Form5()
         {
             InitializeComponent();
+            rbRectángulo.Checked = true;
+        }
+
+        private void btnArea_Click(object sender, EventArgs e)
+        {
+            if (txtAlto.Enabled)
+            {
+                if (rbTriangulo.Checked)
+                {
+                    double res = area(double.Parse(txtAlto.Text), double.Parse(txtAncho.Text), 0);
+                    MessageBox.Show("El area del triángulo es: " + res, "AREA DE TRIANGULO", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                else
+                {
+                    double res = area(double.Parse(txtAlto.Text), double.Parse(txtAncho.Text), 1);
+                    MessageBox.Show("El area del rectángulo es: " + res, "AREA DE RECTÁNGULO", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                double res = area(double.Parse(txtAncho.Text), -1, 0);
+                MessageBox.Show("El area del cuadrado es: " + res, "AREA DE CUADRADO", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        double area(double a, double b, int i)
+        {
+            if (b == -1)
+            {
+                return a * a;
+            }
+            else if (i == 1)
+            {
+                return b * a;
+            }
+            else
+            { 
+                return (b*a)/2;
+            }
+        }
+
+        private void rbTriangulo_CheckedChanged(object sender, EventArgs e)
+        {
+            lblAlto.Text = "Alto: ";
+            lblAncho.Text = "Ancho: ";
+            txtAlto.Enabled = true;
+        }
+
+        private void rbRectángulo_CheckedChanged(object sender, EventArgs e)
+        {
+            lblAlto.Text = "Alto: ";
+            lblAncho.Text = "Ancho: ";
+            txtAlto.Enabled = true;
+        }
+
+        private void rbCuadrado_CheckedChanged(object sender, EventArgs e)
+        { 
+            lblAlto.Text = "-";
+            lblAncho.Text = "Lado";
+            txtAlto.Enabled = false;
         }
     }
 }
