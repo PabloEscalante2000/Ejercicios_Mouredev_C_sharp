951a1640f8ec96b977aff5bdbf2089d48208938e
Pablo <pabloescalante200027@gmail.com>
2022-09-11 21:11:16 -0500

    Ejercicio 7 - Resuelto

---
+++ Ejercicios_Mourodev/Form7.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnInvertir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Da un nombre: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(126, 22);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnInvertir
+            // 
+            this.btnInvertir.Location = new System.Drawing.Point(246, 19);
+            this.btnInvertir.Name = "btnInvertir";
+            this.btnInvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnInvertir.TabIndex = 2;
+            this.btnInvertir.Text = "Invertir";
+            this.btnInvertir.UseVisualStyleBackColor = true;
+            this.btnInvertir.Click += new System.EventHandler(this.btnInvertir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cadena Invertida: ";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(143, 66);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResul.Location = new System.Drawing.Point(146, 65);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(45, 15);
+            this.lblResul.TabIndex = 6;
+            this.lblResul.Text = "            ";
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 111);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnInvertir);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form7";
+            this.Text = "Inviertiendo Cadenas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnInvertir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResul;
+    }
+}
---
+++ Ejercicios_Mourodev/Form7.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvertir_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("No puedes dejar la cadena vacía", "ERROR", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                txtNombre.Focus();
+            }
+            else
+            {
+                lblResul.Text = Invertir(txtNombre.Text.Trim());
+                txtNombre.Focus();
+                lblResul.ForeColor = Color.BlueViolet;
+            }
+        }
+
+        string Invertir(string nombre)
+        {
+            string res = "";
+
+            for (int i = nombre.Length - 1; i >= 0; i--)
+            {
+                res += nombre[i];
+                Console.WriteLine(nombre[i]);
+            }
+
+            return res;
+        }
+    }
+}
