cc258210b381433f26a0d50f7c8ef9d35968896b
Pablo <pabloescalante200027@gmail.com>
2022-09-18 10:15:59 -0500

    Ejercicio 16 - Resuelto

---
+++ Ejercicios_Mourodev/Form16.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form16
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpFecha1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpFecha2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDiferencia = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpFecha1
+            // 
+            this.dtpFecha1.Location = new System.Drawing.Point(75, 8);
+            this.dtpFecha1.Name = "dtpFecha1";
+            this.dtpFecha1.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "fecha1:";
+            // 
+            // dtpFecha2
+            // 
+            this.dtpFecha2.Location = new System.Drawing.Point(75, 51);
+            this.dtpFecha2.Name = "dtpFecha2";
+            this.dtpFecha2.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "fecha2: ";
+            // 
+            // btnDiferencia
+            // 
+            this.btnDiferencia.Location = new System.Drawing.Point(16, 94);
+            this.btnDiferencia.Name = "btnDiferencia";
+            this.btnDiferencia.Size = new System.Drawing.Size(75, 23);
+            this.btnDiferencia.TabIndex = 4;
+            this.btnDiferencia.Text = "Diferencia";
+            this.btnDiferencia.UseVisualStyleBackColor = true;
+            this.btnDiferencia.Click += new System.EventHandler(this.btnDiferencia_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Días: ";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Location = new System.Drawing.Point(56, 139);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(78, 15);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "                       ";
+            // 
+            // Form16
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 168);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDiferencia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpFecha2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpFecha1);
+            this.Name = "Form16";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diferencia entre fechas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFecha1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFecha2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDiferencia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ Ejercicios_Mourodev/Form16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form16 : Form
+    {
+        public Form16()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDiferencia_Click(object sender, EventArgs e)
+        {
+            var res = (int)Math.Abs(dtpFecha1.Value.Day - dtpFecha2.Value.Day);
+
+            lblResultado.Text = res.ToString();
+        }
+    }
+}
