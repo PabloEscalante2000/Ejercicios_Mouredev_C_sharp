586c8e204fa46b0285380cb9ddf37120a0c61da3
Pablo <pabloescalante200027@gmail.com>
2022-09-17 20:54:58 -0500

    Ejercicio 10 - Resuelto

---
+++ Ejercicios_Mourodev/Form10.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.btnTransformar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMorse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "texto normal:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtTexto.Location = new System.Drawing.Point(86, 10);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(409, 20);
+            this.txtTexto.TabIndex = 1;
+            // 
+            // btnTransformar
+            // 
+            this.btnTransformar.Location = new System.Drawing.Point(16, 47);
+            this.btnTransformar.Name = "btnTransformar";
+            this.btnTransformar.Size = new System.Drawing.Size(75, 23);
+            this.btnTransformar.TabIndex = 2;
+            this.btnTransformar.Text = "Transformar";
+            this.btnTransformar.UseVisualStyleBackColor = true;
+            this.btnTransformar.Click += new System.EventHandler(this.btnTransformar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "clave morse:";
+            // 
+            // lblMorse
+            // 
+            this.lblMorse.AutoSize = true;
+            this.lblMorse.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMorse.Location = new System.Drawing.Point(90, 96);
+            this.lblMorse.Name = "lblMorse";
+            this.lblMorse.Size = new System.Drawing.Size(282, 15);
+            this.lblMorse.TabIndex = 4;
+            this.lblMorse.Text = "                                                                                 " +
+    "          ";
+            // 
+            // Form10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 450);
+            this.Controls.Add(this.lblMorse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTransformar);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form10";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "A código morse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Button btnTransformar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMorse;
+    }
+}
---
+++ Ejercicios_Mourodev/Form10.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form10 : Form
+    {
+        List<String> abc = new List<String> { "A","B","C","CH","D","E","F","G","H","I","J","K",
+        "L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6",
+        "7","8","9",".",",","?","\"","/"};
+
+        List<String> abc_morse = new List<String> { "·—", "—···", "—·—·",
+        "————","—··",".","··—·","——·","····","··","·———","—·—",
+        "·—··","——","—·","——·——","———","·——·","——·—","·—·","···",
+        "—","··—","···—","·——","—··—","—·——","——··","—————",
+        "·————","··———","···——","····—","·····","—····","——···","———··",
+        "————·","·—·—·—","——··——","··——··","·—··—·","—··—·"};
+
+        public Form10()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTransformar_Click(object sender, EventArgs e)
+        {
+            String palabra = txtTexto.Text.Trim();
+            String res = "";
+
+            if (palabra == String.Empty)
+            {
+                MessageBox.Show("Tienes que ingresar texto");
+            }
+            else
+            {
+                foreach (char ch in palabra)
+                {
+                    if (abc.Contains(Char.ToString(ch)))
+                    {
+                        int i = abc.IndexOf(Char.ToString(ch));
+                        res = res + abc_morse[i] + " ";
+                    }
+                    else if (ch == ' ')
+                    {
+                        res += "  ";
+                    }
+                    else
+                    {
+                        res += ch;
+                    }
+                }
+            }
+
+            lblMorse.Text = res;
+
+        }
+    }
+}
