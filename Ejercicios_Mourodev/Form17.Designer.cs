736ea939b4457e359f4571b2292e169758e360e4
Pablo <pabloescalante200027@gmail.com>
2022-09-18 17:30:05 -0500

    Ejercicio 17 - Resuelto

---
+++ Ejercicios_Mourodev/Form17.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form17
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "En minusculas: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "En mayúsculas: ";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(124, 13);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(224, 20);
+            this.txtMin.TabIndex = 2;
+            this.txtMin.TextChanged += new System.EventHandler(this.txtMin_TextChanged);
+            // 
+            // txtMay
+            // 
+            this.txtMay.Location = new System.Drawing.Point(124, 41);
+            this.txtMay.Name = "txtMay";
+            this.txtMay.Size = new System.Drawing.Size(224, 20);
+            this.txtMay.TabIndex = 3;
+            // 
+            // Form17
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 79);
+            this.Controls.Add(this.txtMay);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form17";
+            this.Text = "Form17";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMay;
+    }
+}
---
+++ Ejercicios_Mourodev/Form17.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form17 : Form
+    {
+        public Form17()
+        {
+            InitializeComponent();
+            txtMay.Enabled = false;
+        }
+
+        private void txtMin_TextChanged(object sender, EventArgs e)
+        {
+            string res = "";
+            
+            foreach (char item in txtMin.Text)
+            { 
+                res += char.ToUpper(item);
+            }
+
+            txtMay.Text = res;
+        }
+    }
+}
