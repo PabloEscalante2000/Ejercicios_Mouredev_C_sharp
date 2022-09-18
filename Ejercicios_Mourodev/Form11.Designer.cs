15ae96aaf99b01bef9de1ca8de0aa88afe48d075
Pablo <pabloescalante200027@gmail.com>
2022-09-17 21:37:51 -0500

    Ejercicio 11 - Resuelto

---
+++ Ejercicios_Mourodev/Form11.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            this.btnEquilibrada = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ESCRIBE LA FUNCIÓN";
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Location = new System.Drawing.Point(139, 10);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(461, 20);
+            this.txtExpresion.TabIndex = 1;
+            // 
+            // btnEquilibrada
+            // 
+            this.btnEquilibrada.Location = new System.Drawing.Point(16, 41);
+            this.btnEquilibrada.Name = "btnEquilibrada";
+            this.btnEquilibrada.Size = new System.Drawing.Size(117, 23);
+            this.btnEquilibrada.TabIndex = 2;
+            this.btnEquilibrada.Text = "¿Está equilibrada?";
+            this.btnEquilibrada.UseVisualStyleBackColor = true;
+            this.btnEquilibrada.Click += new System.EventHandler(this.btnEquilibrada_Click);
+            // 
+            // Form11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 80);
+            this.Controls.Add(this.btnEquilibrada);
+            this.Controls.Add(this.txtExpresion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form11";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "¿La funcion está equilibrada?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtExpresion;
+        private System.Windows.Forms.Button btnEquilibrada;
+    }
+}
---
+++ Ejercicios_Mourodev/Form11.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form11 : Form
+    {
+        public Form11()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEquilibrada_Click(object sender, EventArgs e)
+        {
+            string expresion = txtExpresion.Text.Trim();
+            List<char> expresionList = new List<char> { '{','}','[',']','(',')'};    
+            List<char> llaves = new List<char>();
+
+            foreach (char c in expresion)
+            {
+                if (expresionList.Contains(c))
+                { 
+                    llaves.Add(c);
+                }
+            }
+
+            while (llaves.Count % 2 == 0 & llaves.Count != 0)
+            {
+                int prob = llaves.Count;
+                for (int i = 0; i < llaves.Count-1; i++)
+                {
+                    if ((llaves[i] == '(' & llaves[i + 1] == ')') | (llaves[i] == '[' & llaves[i + 1] == ']') |
+                        (llaves[i] == '{' & llaves[i + 1] == '}'))
+                    {
+                        llaves.RemoveAt(i);
+                        llaves.RemoveAt(i);
+                        break;
+                    }
+                }
+                if (prob == llaves.Count)
+                { 
+                    break;
+                }
+            }
+
+            if (llaves.Count != 0)
+            {
+                MessageBox.Show("Expresion No equilibrada", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Expresion equilibrada!!", "YAY!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+
+        }
+    }
+}
