2baf3fe6f675e5e508f641737aa1c9a4e452c99c
Pablo <pabloescalante200027@gmail.com>
2022-09-23 21:31:10 -0500

    Ejercicio 18 - Resuelto (Con Alteraciones)

---
+++ Ejercicios_Mourodev/Form18.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Ejercicios_Mourodev
+{
+    partial class Form18
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbMovimiento = new System.Windows.Forms.ComboBox();
+            this.lstMovimientos = new System.Windows.Forms.ListBox();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnResetear = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCompletar = new System.Windows.Forms.Button();
+            this.lblCarrera = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbMovimiento
+            // 
+            this.cbMovimiento.FormattingEnabled = true;
+            this.cbMovimiento.Location = new System.Drawing.Point(15, 19);
+            this.cbMovimiento.Name = "cbMovimiento";
+            this.cbMovimiento.Size = new System.Drawing.Size(148, 21);
+            this.cbMovimiento.TabIndex = 0;
+            // 
+            // lstMovimientos
+            // 
+            this.lstMovimientos.FormattingEnabled = true;
+            this.lstMovimientos.Location = new System.Drawing.Point(14, 58);
+            this.lstMovimientos.Name = "lstMovimientos";
+            this.lstMovimientos.Size = new System.Drawing.Size(315, 212);
+            this.lstMovimientos.TabIndex = 1;
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(169, 18);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionar.TabIndex = 2;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.cbMovimiento);
+            this.groupBox1.Controls.Add(this.btnSeleccionar);
+            this.groupBox1.Controls.Add(this.lstMovimientos);
+            this.groupBox1.Location = new System.Drawing.Point(12, 189);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 290);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selecciona tu lista";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(250, 18);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnResetear);
+            this.groupBox2.Controls.Add(this.lblResultado);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnCompletar);
+            this.groupBox2.Controls.Add(this.lblCarrera);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(342, 171);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Empezar Carrera";
+            // 
+            // btnResetear
+            // 
+            this.btnResetear.Location = new System.Drawing.Point(18, 130);
+            this.btnResetear.Name = "btnResetear";
+            this.btnResetear.Size = new System.Drawing.Size(75, 23);
+            this.btnResetear.TabIndex = 5;
+            this.btnResetear.Text = "Resetear";
+            this.btnResetear.UseVisualStyleBackColor = true;
+            this.btnResetear.Click += new System.EventHandler(this.btnResetear_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Location = new System.Drawing.Point(85, 96);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(39, 15);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "          ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado: ";
+            // 
+            // btnCompletar
+            // 
+            this.btnCompletar.Location = new System.Drawing.Point(15, 55);
+            this.btnCompletar.Name = "btnCompletar";
+            this.btnCompletar.Size = new System.Drawing.Size(75, 23);
+            this.btnCompletar.TabIndex = 2;
+            this.btnCompletar.Text = "Completar";
+            this.btnCompletar.UseVisualStyleBackColor = true;
+            this.btnCompletar.Click += new System.EventHandler(this.btnCompletar_Click);
+            // 
+            // lblCarrera
+            // 
+            this.lblCarrera.AutoSize = true;
+            this.lblCarrera.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCarrera.Location = new System.Drawing.Point(85, 26);
+            this.lblCarrera.Name = "lblCarrera";
+            this.lblCarrera.Size = new System.Drawing.Size(36, 15);
+            this.lblCarrera.TabIndex = 1;
+            this.lblCarrera.Text = "         ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Carrera: ";
+            // 
+            // Form18
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 487);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form18";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Carrera de Obstáculos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbMovimiento;
+        private System.Windows.Forms.ListBox lstMovimientos;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnResetear;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCompletar;
+        private System.Windows.Forms.Label lblCarrera;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Ejercicios_Mourodev/Form18.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_Mourodev
+{
+    public partial class Form18 : Form
+    {
+        public Form18()
+        {
+            InitializeComponent();
+            
+
+            // Otorgando propiedades al combo box
+            List<string> list = new List<string>();
+            list.Add("run");
+            list.Add("jump");            
+            cbMovimiento.DataSource = list;
+            cbMovimiento.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Generar carrera
+            lblCarrera.Text = Generar_Carrera();
+            
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            lstMovimientos.Items.Add(cbMovimiento.SelectedItem.ToString());
+        }
+
+        string Generar_Carrera()
+        {
+            string carrera = "";
+            Random random = new Random();
+            int cont = (int)(random.NextDouble() * 30);
+
+            for (int i = 0; i <= cont; i++)
+            {
+                if (random.NextDouble() > 0.5)
+                {
+                    carrera += "_ ";
+                }
+                else
+                {
+                    carrera += "| ";
+                }
+            }
+
+            return carrera;
+        }
+
+        private void btnResetear_Click(object sender, EventArgs e)
+        {
+            lblCarrera.Text = Generar_Carrera();
+            lblResultado.Text = "          ";
+            lstMovimientos.Items.Clear();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lstMovimientos.Items.Clear();
+        }
+
+        private void btnCompletar_Click(object sender, EventArgs e)
+        {
+            string carrera = lblCarrera.Text.Replace(" ", "");
+
+            List<string> lista = new List<string>();
+
+            string resultado = "";
+            int errores = 0;
+
+            foreach (string i in lstMovimientos.Items)
+            { 
+                lista.Add(i);
+            }
+
+            for (int i = 0; i < Math.Min(lista.Count,carrera.Length); i++)
+            {
+                Console.WriteLine(i);
+
+                if (lista.ElementAt(i) == "run" & carrera[i].Equals('_'))
+                {
+                    resultado += "_ ";
+                }
+                else if (lista.ElementAt(i) == "run" & carrera[i].Equals('|'))
+                {
+                    resultado += "/ ";
+                    errores++;
+                }
+                else if (lista.ElementAt(i) == "jump" & carrera[i].Equals('|'))
+                {
+                    resultado += "| ";
+                }
+                else if (lista.ElementAt(i) == "jump" & carrera[i].Equals('_'))
+                { 
+                    resultado += "x ";
+                    errores++;
+                }
+            }
+
+            if (resultado.Length/2 < carrera.Length)
+            {
+                MessageBox.Show("No pudiste completar la carrera!, y además tuviste: " + errores + " errores", "Sorryyyy!");
+            }
+            else
+            {
+                MessageBox.Show("Completaste la carrera!, obteniendo: " + errores + " errores", "Yupi!!");
+            }
+
+            lblResultado.Text = resultado;
+        }
+    }
+}
